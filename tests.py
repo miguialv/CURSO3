2f366c998193c99b40bab219411663514de87d3d
Migui <alvareznistalmiguel@gmail.com>
2021-03-24 11:39:23 +0100

    checkpoint 4

--- tests.py
+++ tests.py
@@ -9,7 +9,16 @@ def test_anyo(self):
     number = [x1:x2]
     self.assertEqual(l1.get_anyo(), number) 
 
-def test_miFuncion(self):
+
+def test_mas_antiguos(self):
+    lista = ["Hola","Adios","Jamas"]
+    anyo = 2014 
+    l1 = Libro(autor= Autor(id_autor = "1", nombre = "Miguel", apellido = "Alvarez"), titulo = "Hola", anyo = 2005)
+    l2 = Libro(autor= Autor(id_autor = "2", nombre = "Manuel", apellido = "Alvarez"), titulo = "Adios", anyo = 2016)
+    l3 = Libro(autor= Autor(id_autor = "3", nombre = "Marcos", apellido = "Jimenez"), titulo = "Jamas", anyo = 2015)
+    self.assertEqual(l1.mas_antiguos(lista,anyo).get_NombreAutor, "Miguel")
+    self.assertEqual(l1.mas_antiguos(lista,anyo), "Hola")
+    self.assertEqual(l1.mas_antiguos(lista,anyo).get_anyo, "2005")
 
 
 def test_libro(self):
